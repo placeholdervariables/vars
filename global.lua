e1fe0ca8f8ad1ae9b353bf1fad28c390419156ef
placeholdervariables <burnerdeademail2@gmail.com>
2024-06-07 03:48:08 -0400

    Update global.lua

--- global.lua
+++ global.lua
@@ -7,7 +7,7 @@ for i,v in next, game.GetChildren(game) do
 end
 
 GetBotGlobal.InitiatedPrintMessage = "Hi, if you see this in console then the global loadstring has been initiated!"
-GetBotGlobal.BotMessageChat = " @murderstab on ig | my story is heat come give me views and hearts BITCH"
+GetBotGlobal.BotMessageChat = " @murderstab on ig | my st#ry is heat come give me views and hearts BITCH"
 
 GetBotGlobal.wait = task.wait
 GetBotGlobal.spawn = task.spawn
